5734fcb77f627e0957a966ec2454cc8bc09de04b
jamievullo <jvullo@protonmail.com>
2019-11-11 07:43:15 -0500

    Added helper methods to category controller that were removed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
 
+    helper_method :sort_column, :sort_direction
+    
     def index 
         @categories = Category.order(sort_column + " " + sort_direction)
     end
